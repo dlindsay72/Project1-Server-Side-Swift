912b6a2a56e17083c6a91523f19d319eabb4503e
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-27 19:21:02 -0500

    Copied contents of Views and public from course folders which contain Bootstrap css, font and js info

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,6 +6,7 @@ import KituraStencil
 HeliumLogger.use(.info)
 let router = Router()
 router.setDefault(templateEngine: StencilTemplateEngine())
+router.all("/static", middleware: StaticFileServer())
 
 router.get("/") {
    request, response, next in
